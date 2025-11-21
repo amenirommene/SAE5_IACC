bad770f529b4db785c7f1802ef96ef78f7868672
amenir88 <ameni.rommene@esprit.tn>
2025-11-21 09:10:06 +0100

    Workshop 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionsComponent } from './core/list-suggestions/list-suggestions.component';
+
+@NgModule({
+  declarations: [ //les composants liés à ce module
+    AppComponent, HeaderComponent, FooterComponent, ListSuggestionsComponent
+  ],
+  imports: [ //les packages dont on a besoin dans ce module
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
