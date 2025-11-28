dc70e87f35e1e72e0146bb515cbaeb7686a92f0f
amenir88 <ameni.rommene@esprit.tn>
2025-11-28 12:14:15 +0100

    Routage statique et lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { HomeComponent } from './core/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListSuggestionsComponent } from './features/suggestions/list-suggestions/list-suggestions.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) },
+  { path: 'suggestions', loadChildren: () => import('./features/suggestions/suggestions.module').then(m => m.SuggestionsModule) },
+  //il faut que cette route soit la dernière
+   {path:"**", component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { ListSuggestionsComponent } from './core/list-suggestions/list-suggestions.component';
+import { ListSuggestionsComponent } from './features/suggestions/list-suggestions/list-suggestions.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './core/home/home.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 @NgModule({
   declarations: [ //les composants liés à ce module
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ListSuggestionsComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [ //les packages dont on a besoin dans ce module
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
